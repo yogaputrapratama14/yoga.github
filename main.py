3a75175a4907a26ab0efe36bb8183d4da871edd7
yogaputrapratama14 <154648995+yogaputrapratama14@users.noreply.github.com>
2024-05-30 11:53:04 +0700

    Add files via upload

---
+++ main.py
@@ -0,0 +1,25 @@
+import cv2
+
+face_ref = cv2.CascadeClassifier("eyes_ref.xml")
+camera = cv2.VideoCapture(0)
+
+def face_detection(frame):
+    optimized_frame = cv2.cvtColor(frame, cv2.COLOR_RGB2GRAY)
+    faces = face_ref.detectMultiScale(optimized_frame, scaleFactor=1.1)
+    return faces
+
+def drawer_box(frame):
+    for x, y, w, h in face_detection(frame):
+        cv2.rectangle(frame, (x, y), (x + w, y + h),(0, 255, 0), 4)
+
+def main():
+    while True:
+        _, frame = camera.read()
+        drawer_box(frame)
+        cv2.imshow("Yoga Cam", frame)
+        
+        if cv2.waitKey(1)& 0xFF == ord('k'):
+            break
+
+if __name__ =='__main__':
+    main()
